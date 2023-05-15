3b6bbcf396d26303a79e4e30e2be161dd8f7601e
mark garcia <markkusag@outlook.com>
2023-05-15 17:05:20 -0500

    intitial

---
+++ person.js
@@ -0,0 +1,36 @@
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  introduce() {
+    return `Hi, I'm ${this.firstName} ${this.lastName} and I'm ${this.age} years old.`;
+  }
+  static introducePeople(people) {
+    if (!Array.isArray(people)) {
+      console.log('introducePeople only takes an array as an argument.');
+      return;
+    }
+    for (const person of people) {
+      if (!(person instanceof Person)) {
+        console.log('All items in array must be Person class instances.');
+        return;
+      }
+    }
+    const introductions = people.map(person => person.introduce());
+    console.log(introductions);
+  }
+ }
+ const newGuy = new Person('John', 'Doe', '26');
+ const myself = new Person('Mark', 'Garcia', '24');
+ const invalidPerson = {};
+ Person.introducePeople([newGuy, myself]);
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Person;
+} catch {
+  module.exports = null;
+}
